1a56e4a8bd6efa1f9f6e7feb6aa99c5f99969ca1
Isaiah Brewer <ibrew919@gmail.com>
2024-12-10 17:28:56 -0700

    Change

---
+++ README.md
@@ -0,0 +1,29 @@
+# Kanban Authentication
+
+This application uses JWT to authenticate a user logging in to a To-Do list.
+
+## Installation
+
+
+```python
+npm i
+
+npm run build
+
+npm run start
+```
+
+## Example Site
+
+https://kanban-authentication.onrender.com
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ server/src/server.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { sequelize } from './models/index.js';
+import authRoutes from './routes/auth-routes.js';
+import { ticketRouter } from './routes/api/ticket-routes.js'; // Import ticket routes
+import { authenticateToken } from './middleware/auth.js';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const forceDatabaseRefresh = false; // Set this to true if you want to force a database refresh
+
+// Serves static files in the entire client's dist folder
+app.use(express.static('../client/dist'));
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+// Use auth routes
+app.use('/api/auth', authRoutes);
+
+// Use ticket routes
+app.use('/api/tickets', authenticateToken, ticketRouter);
+
+// Add routes that require authentication here
+app.get('/api/protected', authenticateToken, (_req: Request, res: Response) => {
+    res.json({ message: "This is a protected route." });
+});
+
+sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+});
